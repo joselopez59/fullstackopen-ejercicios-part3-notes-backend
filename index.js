cafa431b0f9ad389db1a4e57d0fd6c2473837f2a
Jose <mail@joselopez.de>
2021-12-14 15:34:15 +0100

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { request, response } = require('express');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 /* Daten */
@@ -25,6 +26,7 @@ let notes = [
 ];
 
 /* middleware */
+app.use(cors());
 app.use(express.json());
 
 const requestLogger = (request, response, next) => {
