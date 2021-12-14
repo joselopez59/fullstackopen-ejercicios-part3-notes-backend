d38dbdf8de608a6ea4f30c309f348acfc2b9ac97
Jose <mail@joselopez.de>
2021-12-14 18:25:22 +0100

    remove cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { request, response } = require('express');
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
 /* Daten */
