8752dd60ae26db8007915cf2933bae8b273205a7
Jose <mail@joselopez.de>
2021-12-19 23:21:16 +0100

    uibuild

---
+++ README.md
@@ -0,0 +1,3 @@
+Heroku:
+secret-headland-74777
+https://secret-headland-74777.herokuapp.com/
--- index.js
+++ index.js
@@ -1,45 +1,13 @@
+require('dotenv').config();
 const express = require('express');
-var morgan = require('morgan');
-// const cors = require('cors');
-const app = express();
+const morgan = require('morgan');
 
-/* Daten */
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-];
+const app = express();
+const Note = require('./models/note');
 
 /* middleware */
 app.use(express.static('build'));
-// app.use(cors());
 app.use(express.json());
-
-// const requestLogger = (request, response, next) => {
-//   console.log('Method:', request.method);
-//   console.log('Path:  ', request.path);
-//   console.log('Body:  ', request.body);
-//   console.log('---')
-
-//   next();
-// }
-
-// app.use(requestLogger);
 
 morgan.token('body', (req, res) => `body: ${JSON.stringify(req.body)}`);
 
@@ -60,66 +28,95 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes);
+  
+  Note.find({}).then(notes => {
+    response.json(notes);
+  });
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find(note => note.id === id);
-
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(400).end();
-  }
+app.get('/api/notes/:id', (request, response, next) => {
+  
+  Note.findById(request.params.id).then(note => {
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  })
+  // .catch(error => {
+  //   console.log('error', error);
+  //   response.status(400).send({ error: 'malformatted id' });
+  // })
+  .catch(error => next(error))
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter(note => note.id !== id);
+app.delete('/api/notes/:id', (request, response, next) => {
 
-  response.status(204).end();
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => response.status(204).end())
+    .catch(error => next(error))
+
 });
-
-const generateId = () => {
-  const maxId = notes.length > 0
-  ? Math.max(...notes.map(n => n.id))
-  : 0
-
-  return maxId + 1;
-}
 
 app.post('/api/notes', (request, response) => {
  
   const body = request.body;
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: 'content missing'
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
-  // console.log('note :>> ', note);
-  notes = notes.concat(note);
-
-  response.json(note);
+    date: new Date()
+  });
+  
+  note.save().then(savedNote => {
+    response.json(note);
+  });
 });
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  .then(updatedNote => response.json(updatedNote))
+  .catch(error => next(error))
+});
+
+/* middleware postroutes */
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ 
     error: 'unknown endpoint' 
   });
 }
-
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint);
 
+const errorHandler =(error, request, response, next) => {
+  console.log('errorHandler');
+  console.error(error.message);
+
+  if(error.name  === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// handler of requests with result to errors
+app.use(errorHandler);
+
 /* Server */
-const PORT = process.env.PORT ||3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
